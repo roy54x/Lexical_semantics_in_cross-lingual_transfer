c08138d130bc840abd8bfe567aaada53503b5109
Roy.ilani <Roy.ilani@vayyar.com>
2023-12-04 16:31:39 +0200

    check "golden_collection" alignments

--- compare_aligners.py
+++ compare_aligners.py
@@ -14,12 +14,11 @@ def read_conllu_sentences(file_path):
     return sentences
 
 
-def get_alignments_as_tuples(alignments_dic):
-    alignments_as_tuples = []
-    for key, values in alignments_dic.items():
-        for value in values:
-            if key != "X" and value != "X":
-                alignments_as_tuples.append((int(key)-1, int(value)-1))
+def get_alignments_as_tuples(en_es_data):
+    alignments_as_tuples = [[] for i in range(100)]
+    for alignment in en_es_data:
+        sentence_idx, source, target, _ = alignment.split()
+        alignments_as_tuples[int(sentence_idx)-1].append((int(source)-1, int(target)-1))
     return alignments_as_tuples
 
 
@@ -40,25 +39,26 @@ def calculate_f1_score(gold_alignments, predicted_alignments):
     return f1, true_positive, false_positive, false_negative
 
 
-en_sentences = read_conllu_sentences(r"C:\Users\RoyIlani\pythonProject\data\UD_data\en_pud-ud-test.conllu")
-es_sentences = read_conllu_sentences(r"C:\Users\RoyIlani\pythonProject\data\UD_data\es_pud-ud-test.conllu")
+en_data = open(r"C:\Users\RoyIlani\pythonProject\data\golden_collection\golden_collection\sentences\1-100-final.en", encoding="utf-8")
+en_sentences = [x.split("> ")[1].split("</s>")[0] for x in en_data]
+es_data = open(r"C:\Users\RoyIlani\pythonProject\data\golden_collection\golden_collection\sentences\1-100-final.es", encoding="utf-8")
+es_sentences = [x.split("> ")[1].split("</s>")[0] for x in es_data]
 
-f = open('pud_data.json', encoding="utf8")
-en_es_data = json.load(f)["en-es"]
-gold_alignments = [get_alignments_as_tuples(i["alignment"]) for i in en_es_data]
+en_es_data = open(r"C:\Users\RoyIlani\pythonProject\data\golden_collection\golden_collection\en-es_1-100.wa")
+gold_alignments = get_alignments_as_tuples(en_es_data)
 gold_alignments = [filter_only_1_to_1_alignment(alignment) for alignment in gold_alignments]
 
 aligner = simalign.SentenceAligner(model="xlmr", token_type="bpe", matching_methods="a", device="cuda:0")
+en_sentences_split = [sentence.split() for sentence in en_sentences]
+es_sentences_split = [sentence.split() for sentence in es_sentences]
 simalign_alignments = [aligner.get_word_aligns(source_words, target_words)['inter']
-                       for source_words, target_words in zip(en_sentences, es_sentences)]
+                       for source_words, target_words in zip(en_sentences_split, es_sentences_split)]
 simalign_alignments = [filter_only_1_to_1_alignment(alignment) for alignment in simalign_alignments]
 
 with open(os.path.join(r"C:\Users\RoyIlani\pythonProject\data\alignments\en-es_alignments", "en-es_alignments_only1to1_clean_inverse_clean_inverse.json"),
           "r") as json_file:
     word_dic = json.load(json_file)
-en_sentences_joined = [" ".join(sentence) for sentence in en_sentences]
-es_sentences_joined = [" ".join(sentence) for sentence in es_sentences]
-_, my_alignments = transform_sentences_semantic(en_sentences_joined, es_sentences_joined, source_language="en",
+_, my_alignments = transform_sentences_semantic(en_sentences, es_sentences, source_language="en",
                                                 target_language="es", word_dic=word_dic, no_lemmas=True)
 
 simalign_f1, simalign_true_positive, simalign_false_positive, simalign_false_negative = calculate_f1_score(gold_alignments, simalign_alignments)
