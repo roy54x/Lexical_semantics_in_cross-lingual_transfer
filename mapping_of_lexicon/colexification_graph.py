30a3e90dd302d69ae56b1d8a5afbbaa41b4311f9
Roy.ilani <Roy.ilani@vayyar.com>
2023-12-05 17:41:13 +0200

    plot graph with a good layout

--- mapping_of_lexicon/colexification_graph.py
+++ mapping_of_lexicon/colexification_graph.py
@@ -10,22 +10,18 @@ from transformations.semantic_transformation import get_inverse_dic
 
 def get_colexification_graph(dic):
     inverse_dic = get_inverse_dic(dic)
-    graph_vertices = list(dic.keys())
-    graph_edges = set()
-    for vertix in graph_vertices:
-        for target_value in dic[vertix].keys():
-            colexifications = inverse_dic[target_value].keys()
-            edges = {(graph_vertices.index(vertix), graph_vertices.index(colexification))
-                     for colexification in colexifications}
-            graph_edges.update(edges)
-    return graph_vertices, graph_edges
-
-
-def plot_graph(graph_vertices, graph_edges):
     G = nx.DiGraph()
-    G.add_nodes_from(graph_vertices)
-    G.add_edges_from(graph_edges)
-    pos = nx.spring_layout(G)
+    source_keys = list(dic.keys())
+    G.add_nodes_from(source_keys)
+    for source_key in source_keys:
+        for target_value in dic[source_key].keys():
+            colexifications = inverse_dic[target_value].keys()
+            G.add_edges_from({(source_key, colexification) for colexification in colexifications})
+    return G
+
+
+def plot_graph(G):
+    pos = nx.kamada_kawai_layout(G)
     nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700)
     plt.show()
 
@@ -34,7 +30,8 @@ if __name__ == '__main__':
     main_dir = ""
     with open(os.path.join(main_dir, "en-es.json"), "r") as json_file:
             lemmas_dic = json.load(json_file)
+    lemmas_dic = dict(list(lemmas_dic.items())[:20])
 
-    graph_vertices, graph_edges = get_colexification_graph(lemmas_dic)
-    plot_graph(graph_vertices, graph_edges)
+    G = get_colexification_graph(lemmas_dic)
+    plot_graph(G)
     print("done")
