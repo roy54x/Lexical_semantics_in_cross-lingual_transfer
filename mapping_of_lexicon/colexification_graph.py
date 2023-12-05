52abc538d830aac32053c59ce81d9c50abbaec07
Roy.ilani <Roy.ilani@vayyar.com>
2023-12-05 17:20:33 +0200

    plot graph added

--- mapping_of_lexicon/colexification_graph.py
+++ mapping_of_lexicon/colexification_graph.py
@@ -1,6 +1,8 @@
-import json
 import os
 import sys
+import json
+import networkx as nx
+import matplotlib.pyplot as plt
 
 sys.path.insert(0, r"C:\Users\RoyIlani\pythonProject\Lexical_semantics_in_cross-lingual_transfer")
 from transformations.semantic_transformation import get_inverse_dic
@@ -19,10 +21,20 @@ def get_colexification_graph(dic):
     return graph_vertices, graph_edges
 
 
+def plot_graph(graph_vertices, graph_edges):
+    G = nx.DiGraph()
+    G.add_nodes_from(graph_vertices)
+    G.add_edges_from(graph_edges)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700)
+    plt.show()
+
+
 if __name__ == '__main__':
     main_dir = ""
     with open(os.path.join(main_dir, "en-es.json"), "r") as json_file:
             lemmas_dic = json.load(json_file)
 
     graph_vertices, graph_edges = get_colexification_graph(lemmas_dic)
+    plot_graph(graph_vertices, graph_edges)
     print("done")
